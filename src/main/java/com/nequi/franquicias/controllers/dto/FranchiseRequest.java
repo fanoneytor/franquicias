8e83078ad0bed518ff9b629a59340b5a352c6ffe
Stefano Arias <stefano9405@hotmail.com>
2025-08-07 15:51:21 -0500

    FEAT/Creacion dtos Franchise

---
+++ src/main/java/com/nequi/franquicias/controllers/dto/FranchiseRequest.java
@@ -0,0 +1,8 @@
+package com.nequi.franquicias.controllers.dto;
+
+import lombok.Data;
+
+@Data
+public class FranchiseRequest {
+    private String name;
+}
---
+++ src/main/java/com/nequi/franquicias/controllers/dto/FranchiseResponse.java
@@ -0,0 +1,9 @@
+package com.nequi.franquicias.controllers.dto;
+
+import lombok.Data;
+
+@Data
+public class FranchiseResponse {
+    private Long id;
+    private String name;
+}
