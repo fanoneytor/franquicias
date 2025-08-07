65d913cd6abaa02f03d8930e2ab062218c1f9fa9
Stefano Arias <stefano9405@hotmail.com>
2025-08-07 16:03:49 -0500

    FIX/Ajuste entidades.

--- src/main/java/com/nequi/franquicias/domain/model/Franchise.java
+++ src/main/java/com/nequi/franquicias/domain/model/Franchise.java
@@ -1,24 +1,43 @@
 package com.nequi.franquicias.domain.model;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Franchise {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, unique = true)
     private String name;
 
     @OneToMany(mappedBy = "franchise", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Branch> branches;
+    private List<Branch> branches = new ArrayList<>();
+
+    public Franchise(String name) {
+        this.name = name;
+    }
+
+    public void updateName(String name) {
+        this.name = name;
+    }
+
+    public void addBranch(Branch branch) {
+        branches.add(branch);
+        branch.setFranchise(this);
+    }
+
+    public void removeBranch(Branch branch) {
+        branches.remove(branch);
+        branch.setFranchise(null);
+    }
 }
